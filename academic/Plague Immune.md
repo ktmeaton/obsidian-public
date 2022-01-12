a8c7f32cf00463c9efedad1e7fba564b12532766
Katherine Eaton <ktmeaton@gmail.com>
2022-01-11 17:37:36 -0700

    finish notes for 2022-01-11

--- academic/Plague Immune.md
+++ academic/Plague Immune.md
@@ -14,13 +14,20 @@ The [[Plague Immune]] project is...
 
 ## Mapping
 
-- [Aarhus Municipality](geo:56.1630291,10.093856472098953) | #plague-immune
-- [Esbjerg Municipality](geo:55.4087918,8.748206338151686) | #plague-immune
-- [Viborg Municipality](geo:56.450894000000005,9.382773996723492) | #plague-immune
-- [Faaborg-Midtfyn Municipality](geo:55.196396050000004,10.4279959085546) #plague-immune 
-- [Lolland Municipality](geo:54.7776205,11.268787701987558) | #plague-immune 
-- [Horsens Municipality](geo:55.933767599999996,9.770203532029441) | #plague-immune 
-- [Greater London](geo:51.4893335,-0.14405508452768728) | #plague-immune 
+- [Aarhus Municipality](geo:56.1630291,10.093856472098953) | Nordby | #plague-immune
+- [Esbjerg Municipality](geo:55.4087918,8.748206338151686) | Ribe Grabodre | #plague-immune
+- [Esbjerg Municipality](geo:55.4087918,8.748206338151686) | Ribe Lindegarden | #plague-immune
+- [Viborg Municipality](geo:56.450894000000005,9.382773996723492) | Sct. Drotten | #plague-immune
+- [Viborg Municipality](geo:56.450894000000005,9.382773996723492) | Sct. Matthias | #plague-immune
+- [Viborg Municipality](geo:56.450894000000005,9.382773996723492) | Sct. Michael | #plague-immune
+- [Viborg Municipality](geo:56.450894000000005,9.382773996723492) | Faldborg | #plague-immune
+- [Faaborg-Midtfyn Municipality](geo:55.196396050000004,10.4279959085546) | Hagerup | #plague-immune 
+- [Lolland Municipality](geo:54.7776205,11.268787701987558) | Refshale | #plague-immune 
+- [Horsens Municipality](geo:55.933767599999996,9.770203532029441) | Sejet | #plague-immune 
+- [Horsens Municipality](geo:55.933767599999996,9.770203532029441) | Ole Wormsgade | #plague-immune 
+- [Horsens Municipality](geo:55.933767599999996,9.770203532029441) | Tirup | #plague-immune 
+- [Horsens Municipality](geo:55.933767599999996,9.770203532029441) | Klosterkirken | #plague-immune 
+- [Greater London](geo:51.4893335,-0.14405508452768728) | East Smithfield | #plague-immune 
 
 ## Tasks
 
---
+++ academic/pandoc/pdf2md.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+import click
+import os
+import fitz
+import re
+import datetime
+
+
+VERSION = "0.1.0"
+# -----------------------------------------------------------------------------
+# Main
+
+@click.command()
+@click.version_option(version=VERSION,)
+@click.option('--pdf'         , default=None, help="PDF input for conversion.")
+@click.option('--output-dir'  , default=".", help="Output directory.")
+
+def main(
+    pdf,
+    output_dir,
+):
+    """Convert PDF to Markdown."""
+
+    # Check if file exists
+    if not os.path.exists(pdf):
+        print("PDF does not exists", pdf)
+        quit()
+
+    # Check if output directory exists
+    if not os.path.exists(output_dir):
+        print("Creating output directory:", output_dir)
+        os.mkdir(output_dir)
+
+    pdf_prefix = os.path.splitext(os.path.basename(pdf))[0]
+    output_file = os.path.join(output_dir, pdf_prefix + ".md")
+
+    print("\n")   
+    print("PDF:     ", pdf)
+    print("Markdown:", output_file)     
+    print("\n")
+
+    # -------------------------------------------------------------------------
+    # YAML Header
+    today = datetime.datetime.today()
+
+    yaml_header = """---
+    - title: \"{title}\"
+    - pdf2md: {version}
+    - status: idea
+    - type: Note
+    - due: {today} 
+    - time: 09:00
+    ---
+    """.format(
+        title=pdf_prefix,
+        version=VERSION,
+        today=today.strftime("%Y-%m-%d")
+    )
+
+    yaml_header = yaml_header.replace("  ","")
+
+    # -------------------------------------------------------------------------
+    # Extract Text
+    pdf_doc = fitz.open(pdf)
+
+    raw_text = ""
+    for page in pdf_doc:
+        page_text = page.get_text()
+
+        # -------------------------------------------------------------------------
+        # Process Text        
+
+        # Retain the literate utf-8 characters
+        page_text = repr(page_text)
+
+        # Remove unicode characters
+        page_text = re.sub(r'[^\x00-\x7F]',' ', page_text)
+
+        # "\n\uf0b7 \n" is a list element
+        page_text = re.sub(r'( )+\\n\\uf0b7 \\n','\\n- ', page_text)      
+
+        # Remove multiple occurences of whitespace
+        page_text = re.sub(r' +',' ', page_text)
+
+        # ' \n 1.' is a list element   
+        # TO BE DONE BETTER
+        for i in range(1,10):
+            page_text = re.sub(r'\\n' + str(i) + '\.','\\n'  + str(i) + '.', page_text)
+
+        # ' \n \n' are true content linebreaks: keep
+        page_text = re.sub(r'\\n \\n','LINEBREAK', page_text)
+
+        # ' \n' are aesthetic linebreaks: remove
+        page_text = re.sub(r' \\n',' ', page_text)
+        page_text = re.sub(r'\\n','', page_text)
+
+        page_text = re.sub(r'( )*LINEBREAK( )*','\n', page_text)
+
+        # Random quotes
+        page_text = page_text.replace("'","").replace('"','')
+
+        # Append
+        raw_text += page_text
+
+    # -------------------------------------------------------------------------
+    # Export
+
+    with open(output_file, "w") as outfile:
+        outfile.write(yaml_header)
+        outfile.write("\n")
+        outfile.write(raw_text)  
+
+
+if __name__ == "__main__":
+    main()
